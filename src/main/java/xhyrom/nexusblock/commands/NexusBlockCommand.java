2c4a2f1925d6ee996d978a09d9b5c38589f03f2f
Sliide_ <27533618+yiyoperez@users.noreply.github.com>
2024-03-25 00:48:10 -0300

    chore: Remove unnecessary message utils method.

--- src/main/java/xhyrom/nexusblock/commands/NexusBlockCommand.java
+++ src/main/java/xhyrom/nexusblock/commands/NexusBlockCommand.java
@@ -3,26 +3,30 @@ package xhyrom.nexusblock.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import xhyrom.nexusblock.NexusBlock;
 
-import static xhyrom.nexusblock.utils.MessageUtils.message;
+import static xhyrom.nexusblock.utils.MessageUtils.translateColorCodes;
 
-public class nexusblock implements CommandExecutor {
-    private String infoMessage = message("Plugin by &cxHyroM#2851 &7available on github &c&nhttps://github.com/xHyroM/NexusBlock");
+public class NexusBlockCommand implements CommandExecutor {
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        String infoMessage = translateColorCodes("Plugin by &cxHyroM#2851 &7available on github &c&nhttps://github.com/xHyroM/NexusBlock");
+
         if (!sender.hasPermission("nexusblock.admin")) {
             sender.sendMessage(infoMessage);
             return true;
         }
 
         if (args.length == 0 || args[0].equalsIgnoreCase("info")) {
-            sender.sendMessage(message(infoMessage + "\n&cAdmin Commands:\n\n&c/nexusblock reload &8- &7Reload plugin", false));
+            sender.sendMessage(translateColorCodes(infoMessage));
+            sender.sendMessage(translateColorCodes("&cAdmin Commands:\n\n&c/nexusblock reload &8- &7Reload plugin"));
+            sender.sendMessage(translateColorCodes("&c/nexusblock reload &8- &7Reload plugin"));
             return true;
         }
 
+        //TODO
         if (args[0].equalsIgnoreCase("reload")) {
-            sender.sendMessage(message("Please restart server."));
+            sender.sendMessage(translateColorCodes("Please restart server."));
             // NexusBlock.getInstance().reloadConfig();
             // NexusBlock.getInstance().onReload();
             return true;
--- src/main/java/xhyrom/nexusblock/utils/MessageUtils.java
+++ src/main/java/xhyrom/nexusblock/utils/MessageUtils.java
@@ -1,20 +1,10 @@
 package xhyrom.nexusblock.utils;
 
 import org.bukkit.ChatColor;
-import xhyrom.nexusblock.NexusBlock;
 
 public class MessageUtils {
-    public static String message(String text) {
-        return message(text, true);
-    }
 
-    public static String message(String text, boolean prefix) {
-        return prefix ?
-                translateColorCodes(NexusBlock.getInstance().config.getString("prefix") + text) :
-                translateColorCodes(text);
-    }
-
-    private static String translateColorCodes(String text) {
+    public static String translateColorCodes(String text) {
         return ChatColor.translateAlternateColorCodes('&', text);
     }
 }
