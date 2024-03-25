c50d0c32bdf82b27445c79593e64a078d6e3a7ad
Sliide_ <27533618+yiyoperez@users.noreply.github.com>
2024-03-25 04:48:08 -0300

    style: Just clean this.

--- src/main/java/xhyrom/nexusblock/structures/holograms/HologramInterface.java
+++ src/main/java/xhyrom/nexusblock/structures/holograms/HologramInterface.java
@@ -4,10 +4,16 @@ import org.bukkit.Location;
 import org.bukkit.inventory.ItemStack;
 
 public interface HologramInterface {
+
     Object createHologram(Location location, int id, double hologramLocation);
+
     void insertTextLine(Object hologram, Integer line, String desc);
+
     void insertItemLine(Object hologram, Integer line, ItemStack item);
+
     void editTextLine(Object hologram, Integer line, String desc, Boolean save);
+
     void editItemLine(Object hologram, Integer line, ItemStack item, Boolean save);
+
     void deleteHologram(Object hologram);
 }
